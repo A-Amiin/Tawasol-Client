db2133acf5a634b78c6087bf77cdd2bf60fca3c8
A-Amiin <ahmed.m.aminn@gmail.com>
2025-02-09 16:40:24 +0200

    Adding the JoinUs Page

--- client/src/pages/JoinUsPage.jsx
+++ client/src/pages/JoinUsPage.jsx
@@ -4,28 +4,28 @@ import LoginComponent from "../components/Login/LogInComponent";
 import RegisterComponent from "../components/Register/RegisterComponent";
 
 const JoinUsPage = () => {
-    const [isLogin, setIsLogin] = useState(true);
+    const [isRegister, setIsRegister] = useState(true);
 
     return (
-        <div className="background-page ">
-            <div className="d-flex flex-column align-items-center justify-content-center">
-                <motion.div
-                    key={isLogin ? "login" : "register"}
-                    initial={{ rotateY: 90, opacity: 0 }}
-                    animate={{ rotateY: 0, opacity: 1 }}
-                    exit={{ rotateY: -90, opacity: 0 }}
-                    transition={{ duration: 0.6, ease: "easeInOut" }}
-                    className="auth-container"
-                >
-                    {isLogin ? <LoginComponent /> : <RegisterComponent />}
-                </motion.div>
-                <button
-                    className="btn btn-outline-primary mt-3"
-                    onClick={() => setIsLogin(!isLogin)}
-                >
-                    {isLogin ? "Create an Account" : "Already have an account? Login"}
-                </button>
-            </div>
+        <div className="background-page d-flex align-items-center flex-column justify-content-center">
+
+            <motion.div
+                key={isRegister ? "register" : "login"}
+                initial={{ rotateY: 90, opacity: 0 }}
+                animate={{ rotateY: 0, opacity: 1 }}
+                exit={{ rotateY: -90, opacity: 0 }}
+                transition={{ duration: 0.6, ease: "easeInOut" }}
+                className="container-lg auth-container"
+            >
+                {isRegister ? <RegisterComponent /> : <LoginComponent />}
+            </motion.div>
+            <button
+                className="btn btn-outline-primary mt-3"
+                onClick={() => setIsRegister(!isRegister)}
+            >
+                <span> {isRegister ? "Already have an account" : "Create one"}</span>
+            </button>
+
         </div>
     );
 
