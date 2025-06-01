6636b7982930b3d3c48126ca4902222b0842aa77
A-Amiin <ahmed.m.aminn@gmail.com>
2025-06-02 01:14:33 +0300

    feat: Revamp HomeAside component with trending topics, suggested contacts, and recent activity sections; enhance styles for improved layout and user experience

--- src/components/HomePage/HomeAside/HomeAside.jsx
+++ src/components/HomePage/HomeAside/HomeAside.jsx
@@ -1,29 +1,70 @@
+import { Heart, MessageCircleMore } from "lucide-react"
 const HomeAside = () => {
     return (
-        <div className="HomeAside-wraperr d-flex flex-column">
-            <div className="HomeAside-Trending MyCard mb-3 p-2">
+        <div className="HomeAside-wraperr d-flex flex-column gap-3">
+            <div className="HomeAside-Trending MyCard mb-3 p-2 d-flex flex-column gap-2">
                 <h6 className="HomeAside-Trending-Title">Trending Topics</h6>
-                <ul className="HomeAside-Trending-List">
-                    <li className="HomeAside-Trending-Item">Item 1</li>
-                    <li className="HomeAside-Trending-Item">Item 2</li>
-                    <li className="HomeAside-Trending-Item">Item 3</li>
-                </ul>
+                <div className="HomeAside-Trending-List d-flex flex-column gap-3">
+                    <div className="HomeAside-Trending-Item d-flex flex-column">
+                        <span className="HomeAside-Trending-Item-Title">#WebDevelopment</span>
+                        <span className="HomeAside-Trending-Item-Count">1.23K Posts</span>
+                    </div>
+                    <div className="HomeAside-Trending-Item d-flex flex-column">
+                        <span className="HomeAside-Trending-Item-Title">#ReactJS</span>
+                        <span className="HomeAside-Trending-Item-Count">2.45K Posts</span>
+                    </div>
+                    <div className="HomeAside-Trending-Item d-flex flex-column">
+                        <span className="HomeAside-Trending-Item-Title">#JavaScript</span>
+                        <span className="HomeAside-Trending-Item-Count">3.67K Posts</span>
+                    </div>
+                </div>
             </div>
-            <div className="HomeAside-Contactions MyCard mb-3 p-2">
+            <div className="HomeAside-Contactions MyCard mb-3 p-2 gap-3">
                 <h6 className="HomeAside-Contactions-Title">Suggested Contacts</h6>
-                <ul className="HomeAside-Contactions-List">
-                    <li className="HomeAside-Contactions-Item">Contact 1</li>
-                    <li className="HomeAside-Contactions-Item">Contact 2</li>
-                    <li className="HomeAside-Contactions-Item">Contact 3</li>
-                </ul>
+                <div className="post-header d-flex justify-content-between">
+                    <div className="user-data d-flex">
+                        <img src="../public/Images/user.jpg" alt="User" className="user-image" />
+                        <div className="info d-flex flex-column px-3">
+                            <span className="username">Ahmed Amin</span>
+                            <span className="post-time">UI & UX Designer</span>
+                        </div>
+                    </div>
+                    <button className="btnn">Follow</button>
+                </div>
+                <div className="post-header d-flex justify-content-between">
+                    <div className="user-data d-flex">
+                        <img src="../public/Images/user.jpg" alt="User" className="user-image" />
+                        <div className="info d-flex flex-column px-3">
+                            <span className="username">Shehab Majed</span>
+                            <span className="post-time">Frontend Developer</span>
+                        </div>
+                    </div>
+                    <button className="btnn">Follow</button>
+                </div>
             </div>
-            <div className="HomeAside-Recent-Activity MyCard mb-3 p-2">
+            <div className="HomeAside-Recent-Activity MyCard mb-3 p-2 gap-3">
                 <h6 className="HomeAside-Recent-Activity-Title">Recent Activity</h6>
-                <ul className="HomeAside-Recent-Activity-List">
-                    <li className="HomeAside-Recent-Activity-Item">Activity 1</li>
-                    <li className="HomeAside-Recent-Activity-Item">Activity 2</li>
-                    <li className="HomeAside-Recent-Activity-Item">Activity 3</li>
-                </ul>
+                <div className="HomeAside-Recent-Activity-List d-flex flex-column px-2 gap-3">
+                    <div className="HomeAside-Recent-Activity-Item d-flex align-items-center">
+                        <div className="icon-wrapper d-flex align-items-center justify-content-center">
+                            <Heart className="action-icon" />
+                        </div>
+                        <div className="Activity-info d-flex flex-column px-3">
+                            <span className="activity-text"><span className="name">Sarah Khalil</span> liked your post</span>
+                            <span className="activity-time">2 mins ago</span>
+                        </div>
+                    </div>
+
+                    <div className="HomeAside-Recent-Activity-Item d-flex align-items-center">
+                        <div className="icon-wrapper d-flex align-items-center justify-content-center">
+                            <MessageCircleMore className="action-icon" />
+                        </div>
+                        <div className="Activity-info d-flex flex-column px-3">
+                            <span className="activity-text"> <span className="name">Ahmed Aziz</span> commented on your post</span>
+                            <span className="activity-time">5 mins ago</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
